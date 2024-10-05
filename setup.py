4ca41951eef679f8105a75fbcdcca25159245184
unknown <aryandhanawade2005@gmail.com>
2024-10-05 23:01:02 +0530

    Update requirements

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name='blackscholesmodel',
+    version='0.1',
+    install_requires=[
+        'streamlit',
+        'numpy==1.23.1',
+        'pandas',
+        'sympy',
+        'setuptools',
+        # Add other dependencies as needed
+    ],
+)
